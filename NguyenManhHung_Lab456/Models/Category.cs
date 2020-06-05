d16429776fd7120f7c422e1f286f6cfc98aee999
Manh Hung <mrhung189@gmail.com>
2020-06-05 13:39:20 +0700

    Them dong ghi chu trong Category

---
+++ NguyenManhHung_Lab456/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NguyenManhHung_Lab456.Models
+{
+    public class Category
+    {
+        //Them ghi chu
+
+    }
+}
